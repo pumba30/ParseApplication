edb0cb29d5df0fd246b73eac68af4cc730798318
pumba30 <pumba303030@yandex.ru>
2015-01-19 14:34:38 +0200

    С постраничным выводом, параметр страницы вводим консольно

--- src/main/java/com/pandasoft/ParseSiteDemo.java
+++ src/main/java/com/pandasoft/ParseSiteDemo.java
@@ -1,34 +1,39 @@
 package com.pandasoft;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 /**
  * Created by pumba30 on 19.01.2015.
  */
 public class ParseSiteDemo {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws IOException {
+        String line;
         ParseBashIm parseBashIm = new ParseBashIm();
         ArrayList<String> list = new ArrayList<String>();
 
-        parseBashIm.setPage(1);
-        System.out.println("---------PAGE 1--------------------");
-        list = parseBashIm.getQuotes();
-        int lengthList = list.size();
-        for (int i = 0; i < lengthList; i++) {
-            System.out.println("##" + (i + 1) + " ########\n"
-                    + list.get(i) + "\n###################\n");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+
+        //TODO проверка ввода (должны быть числа)
+        while ((line = reader.readLine()) != null) {
+            if (line.equalsIgnoreCase("exit")) {
+                System.exit(0);
+            }
+
+            int numberPage = Integer.parseInt(line);
+
+            parseBashIm.setPage(numberPage);
+            System.out.println("---------PAGE " + numberPage + " --------------------");
+            list = parseBashIm.getQuotes();
+            int lengthList = list.size();
+            for (int i = 0; i < lengthList; i++) {
+                System.out.println("##" + (i + 1) + " ########\n"
+                        + list.get(i) + "\n###################\n");
+            }
         }
 
-        System.out.println("---------PAGE 2--------------------");
-        parseBashIm.setPage(2);
-        list = parseBashIm.getQuotes();
-
-        lengthList = list.size();
-
-        for (int i = 0; i < lengthList; i++) {
-            System.out.println("######## " + "Цитата № " + (i + 1)
-                    + " ########\n" + list.get(i) + "\n###################\n");
-        }
     }
 }
